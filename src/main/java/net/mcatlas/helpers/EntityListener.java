b5984f70c973117ac918e9d626ee23e39e409744
drewdorris <contact@dorrisd.com>
2019-12-22 13:10:25 -0500

    log when a user kills a tamed animal

--- src/main/java/net/mcatlas/helpers/EntityListener.java
+++ src/main/java/net/mcatlas/helpers/EntityListener.java
@@ -1,5 +1,6 @@
 package net.mcatlas.helpers;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
@@ -7,9 +8,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.entity.Tameable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
-public class EntityClickListener implements Listener {
+public class EntityListener implements Listener {
 	
 	@EventHandler
 	public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
@@ -23,6 +25,16 @@ public class EntityClickListener implements Listener {
 			if (!player.hasPermission("mcatlas.tamedowner")) return;
 			player.sendMessage(ChatColor.GREEN + "The owner of this creature is " 
 					+ ChatColor.GOLD + tamed.getOwner());
+		}
+	}
+
+	@EventHandler
+	public void onEntityDeath(EntityDeathEvent event) {
+		Player killer = event.getEntity().getKiller();
+		if (event.getEntity() instanceof Tameable) {
+			Tameable tamed = (Tameable) event.getEntity();
+			Bukkit.getLogger().info(killer.getName() + " killed " 
+					+ tamed.getOwner().getName() + "'s " + tamed.getType().name());
 		}
 	}
 
