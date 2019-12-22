1621257a4f5ae97c3471e3a80ee0428281da9b4b
drewdorris <contact@dorrisd.com>
2019-12-22 12:20:31 -0500

    right click creature to see owner

---
+++ src/main/java/net/mcatlas/helpers/EntityClickListener.java
@@ -0,0 +1,28 @@
+package net.mcatlas.helpers;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Tameable;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+
+public class EntityClickListener implements Listener {
+	
+	@EventHandler
+	public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
+		Player player = event.getPlayer();
+		Entity entity = event.getRightClicked();
+		if (!player.getInventory().getItemInMainHand().getType().equals(Material.AIR)) return;
+		if (entity instanceof Tameable) {
+			Tameable tamed = (Tameable) entity;
+			if (!tamed.isTamed()) return;
+			if (tamed.getOwner().equals(player)) return;
+			player.sendMessage(ChatColor.GREEN + "The owner of this creature is " 
+					+ ChatColor.GOLD + tamed.getOwner());
+		}
+	}
+
+}
