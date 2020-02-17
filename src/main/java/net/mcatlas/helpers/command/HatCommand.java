9a227972307912910797a10a4b434f716a49d50d
drewdorris <contact@dorrisd.com>
2020-02-17 16:55:16 -0500

    add hat command

--- src/main/java/net/mcatlas/helpers/command/HatCommand.java
+++ src/main/java/net/mcatlas/helpers/command/HatCommand.java
@@ -2,7 +2,6 @@ package net.mcatlas.helpers.command;
 
 import java.util.Collection;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -23,21 +22,29 @@ public class HatCommand implements CommandExecutor {
 		ItemStack itemInHand = player.getInventory().getItemInMainHand();
 		ItemStack existingHelmet = player.getInventory().getHelmet();
 
+		// if holding nothing
 		if (itemInHand == null || itemInHand.getType() == Material.AIR) {
-			if (existingHelmet == null || existingHelmet.getType() == Material.AIR) {
+			if (existingHelmet == null || existingHelmet.getType() == Material.AIR) { // if nothing on helmet either
 				player.sendMessage(ChatColor.RED + "Hold something to put on your head!");
 				return false;
 			}
 
+			// if has helmet, take helmet off
 			setItem(player, new ItemStack(Material.AIR), existingHelmet);
 			player.sendMessage(ChatColor.GREEN + "Hat has been taken off.");
 			return true;
 		}
 
+		// if hat exists & holding something
 		if (existingHelmet != null && existingHelmet.getType() != Material.AIR) {
 			setItem(player, itemInHand, existingHelmet);
-			player.sendMessage(ChatColor.GREEN + "Hat has been put on, and your previous hat taken off.");
-		} else {
+
+			if (itemInHand.getType() == existingHelmet.getType()) {
+				player.sendMessage(ChatColor.GREEN + "Hat has been taken off.");
+			} else {
+				player.sendMessage(ChatColor.GREEN + "Hat has been put on, and your previous hat taken off.");
+			}
+		} else { // if hat doesn't exist & holding something
 			setItem(player, itemInHand, new ItemStack(Material.AIR));
 			player.sendMessage(ChatColor.GREEN + "Hat has been put on.");
 		}
@@ -47,20 +54,27 @@ public class HatCommand implements CommandExecutor {
 
 	public void setItem(Player player, ItemStack itemInHand, ItemStack existingHelmet) {
 		PlayerInventory inventory = player.getInventory();
+		// if hat stack and item held stack are both a single item, just swap
 		if (itemInHand.getAmount() <= 1 && existingHelmet.getAmount() <= 1) {
 			inventory.setHelmet(itemInHand);
 			inventory.setItemInMainHand(existingHelmet);
-		} else if (itemInHand.getType() == existingHelmet.getType()) {
+		} else if (itemInHand.getType() == existingHelmet.getType()) { // if hat and item held are same type
 			inventory.setHelmet(new ItemStack(Material.AIR));
-			itemInHand.setAmount(itemInHand.getAmount() + 1);
-			inventory.setItemInMainHand(itemInHand);
-		} else {
+			Collection<ItemStack> items = inventory.addItem(existingHelmet).values();
+			for (ItemStack item : items) {
+				if (item == null || item.getType() == Material.AIR) continue;
+				inventory.getLocation().getWorld().dropItem(inventory.getLocation(), item);
+				player.sendMessage(ChatColor.YELLOW + "Your hat has dropped.");
+			}
+		} else { // what usually happens
 			itemInHand.setAmount(itemInHand.getAmount() - 1);
 			inventory.setItemInMainHand(itemInHand);
 			itemInHand.setAmount(1);
 			inventory.setHelmet(itemInHand);
+			// if the inventory is full and we attempt to add items, this collection contains those items
 			Collection<ItemStack> items = inventory.addItem(existingHelmet).values();
 			for (ItemStack item : items) {
+				if (item == null || item.getType() == Material.AIR) continue;
 				inventory.getLocation().getWorld().dropItem(inventory.getLocation(), item);
 				player.sendMessage(ChatColor.YELLOW + "Your hat has dropped.");
 			}
