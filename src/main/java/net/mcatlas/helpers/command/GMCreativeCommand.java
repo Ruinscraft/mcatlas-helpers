454f1b29cf295c6bbddce1e8977d92b9626efb2d
drewdorris <contact@dorrisd.com>
2020-03-15 10:21:41 -0400

    stupid

--- src/main/java/net/mcatlas/helpers/command/GMCreativeCommand.java
+++ src/main/java/net/mcatlas/helpers/command/GMCreativeCommand.java
@@ -13,7 +13,7 @@ public class GMCreativeCommand implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (!(sender instanceof Player)) return false;
-		((Player) sender).setGameMode(GameMode.SPECTATOR);
+		((Player) sender).setGameMode(GameMode.CREATIVE);
 		sender.sendMessage(ChatColor.AQUA + "Gamemode set to Creative");
 		return false;
 	}
