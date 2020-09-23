cbd0da6b02ee6c7d20b2b182e2f575157052c813
drewdorris <contact@dorrisd.com>
2020-09-23 17:48:45 -0400

    add comments

--- src/main/java/net/mcatlas/helpers/LilypadRecipeHandler.java
+++ src/main/java/net/mcatlas/helpers/LilypadRecipeHandler.java
@@ -5,7 +5,6 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Item;
 import org.bukkit.inventory.ItemStack;
 
@@ -23,26 +22,27 @@ public class LilypadRecipeHandler implements Runnable {
     @Override
     public void run() {
         for (World world : plugin.getServer().getWorlds()) {
-            for (Entity entity : world.getEntities()) {
-                if (entity.getType() != EntityType.DROPPED_ITEM) {
-                    continue;
-                }
-
+            for (Entity entity : world.getEntitiesByClass(Item.class)) {
                 Item vine = (Item) entity;
                 ItemStack stack = vine.getItemStack();
 
+                // stack of vines
                 if (stack.getAmount() != 64 || stack.getType() != Material.VINE) {
                     continue;
                 }
 
+                // has to be in water!
                 Location location = vine.getLocation();
-                Collection<? extends Item> nearbyItems = location.getNearbyEntitiesByType(vine.getClass(), 3);
+                if (location.getBlock().getType() != Material.WATER ||
+                        location.add(0, -1, 0).getBlock().getType() != Material.WATER) {
+                    continue;
+                }
+
+                Collection<? extends Item> nearbyItems = location.getNearbyEntitiesByType(Item.class, 2);
                 for (Item sugarCane : nearbyItems) {
-                    if (sugarCane.getType() != EntityType.DROPPED_ITEM) {
-                        continue;
-                    }
                     ItemStack sugarCaneItemStack = sugarCane.getItemStack();
 
+                    // stack of sugar cane
                     if (sugarCaneItemStack.getAmount() != 64 || sugarCaneItemStack.getType() != Material.SUGAR_CANE) {
                         continue;
                     }
@@ -50,8 +50,13 @@ public class LilypadRecipeHandler implements Runnable {
                     UUID vineUUID = vine.getUniqueId();
                     UUID sugarCaneUUID = sugarCane.getUniqueId();
 
-                    int delay = plugin.getRandom().nextInt(100);
+                    // extra delay before growth
+                    int delay = plugin.getRandom().nextInt(120);
+                    // spawn 1 or 2
                     int randAmntOfLillies = plugin.getRandom().nextInt(2) + 1;
+
+                    // run task later to check if the entities are there
+                    // if entities are there, remove them, and spawn the lilypad
                     Bukkit.getScheduler().runTaskLater(plugin, () -> {
                         Entity vineEntity = Bukkit.getEntity(vineUUID);
                         Entity sugarCaneEntity = Bukkit.getEntity(sugarCaneUUID);
